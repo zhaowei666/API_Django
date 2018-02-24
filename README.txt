4eca98929c5065f0903454e20ee22124936fe7c0
Zhaowei Ren <rzwdsg@gmail.com>
2018-02-23 22:48:44 -0500

    changed database to postgresql and added README.txt

---
+++ README.txt
@@ -0,0 +1,15 @@
+Deployment:
+$ ssh -i KEY-PEM-FILE ubuntu@EC2-ENDPOINT
+$ git clone GITHUB-LINK
+$ apt-get update
+$ apt-get install python-pip python-dev build-essential
+$ pip install --upgrade pip
+$ pip install virtualenv
+$ virtualenv env
+$ source env/bin/activate
+$ pip install -r requirements.txt
+$ python manage.py makemigrations
+$ python manage.py migrate
+$ python manage.py runserver
+
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dev_db',
+        'USER': 'dev_user',
+        'PASSWORD': 'dev_password',
+        'HOST': 'postgre-for-flask.cxgukboubuqm.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
