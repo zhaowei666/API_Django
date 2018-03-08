9f98c1a52d1b2b532262e414e1b41ddc59eef4cf
Zhaowei Ren <rzwdsg@gmail.com>
2018-03-07 22:23:56 -0500

    added unit test to movie app

--- movie/tests.py
+++ movie/tests.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+import json
 
 # Create your tests here.
+
+
+class MovieTestCases(TestCase):
+    def test_quotes(self):
+        response = self.client.get('/movie/quotes?query={}'.format('man'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.content)
